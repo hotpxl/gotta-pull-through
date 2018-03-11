f67440ea03d67f6a0cab900c6072786dd63bfa45
Yutian Li <hotpxless@gmail.com>
2018-03-11 16:34:42 -0700

    [master] Add animation.

---
+++ Assets/Scripts/PlayerMotion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotion : MonoBehaviour
+{
+	public float speed;
+	private Rigidbody2D rigidbodyComponent;
+
+	void Start ()
+	{
+		rigidbodyComponent = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate ()
+	{
+		var mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		var rotation = Quaternion.LookRotation (transform.position - mousePosition, Vector3.forward);
+		transform.rotation = rotation;
+		transform.eulerAngles = new Vector3 (0, 0, transform.eulerAngles.z);
+		var input = Input.GetAxis ("Vertical");
+		rigidbodyComponent.AddForce (transform.up * speed * input);
+	}
+}
