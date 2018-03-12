ceac6d26337f1a2de9219255b981b37cebf658fa
Yutian Li <hotpxless@gmail.com>
2018-03-11 17:26:01 -0700

    [master] Set up camera follow.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	public GameObject target;
+	public float smoothTime;
+	public Vector3 offset;
+	private Vector3 velocity = Vector3.zero;
+
+	void FixedUpdate ()
+	{
+		var desired = target.transform.position + offset;
+		transform.position = Vector3.SmoothDamp (transform.position, desired, ref velocity, smoothTime);
+	}
+}
