e04f38901f4e42b9fd377365c546f6ab2749b474
Yutian Li <hotpxless@gmail.com>
2018-03-19 18:12:39 -0700

    [master] Arrow that points to finish.

---
+++ Assets/Scripts/PointToFinish.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointToFinish : MonoBehaviour
+{
+	public GameObject target;
+	public float distance = 1f;
+
+	void Update ()
+	{
+		var direction = target.transform.position - transform.parent.position;
+		direction.Normalize ();
+		direction *= distance;
+		transform.position = transform.parent.position + direction;
+		// 90 degrees to offset the rotation of the sprite itself.
+		transform.rotation = Quaternion.AngleAxis (-Vector3.Angle (Vector3.up, direction) + 90, Vector3.forward);
+	}
+}
